9e10d36fe184db62f06e17e060659409a1d59556
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-03-01 17:07:46 -0300

    modularizando

--- app.py
+++ app.py
@@ -1,31 +1,10 @@
-import requests
 from flask import Flask, render_template, request, redirect, url_for, flash
-import os
-from dataclasses import dataclass
-from dotenv import load_dotenv
+from weather import get_weather_data
 
-load_dotenv()
-API_KEY = os.getenv('API_KEY')
 app = Flask(__name__)
 
-@dataclass
-class WeatherData:
-    '''Objeto de resposta da API'''
-    main: str
-    description: str
-    icon: str
-    temperature: float
 
-def get_weather_data(city):
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&lang=pt_br'
-    response = requests.get(url).json()
-    data = WeatherData(
-        main=response.get('weather')[0].get('main'), # Definição do clima, em inglês
-        description=response.get('weather')[0].get('description'),
-        icon=response.get('weather')[0].get('icon'),
-        temperature=response.get('main').get('temp')
-    )
-    return data
+
 
 # Rota Principal
 @app.route('/')
@@ -37,7 +16,6 @@ def index_post():
     msg_erro = ''
     city = request.form.get('cidade') # GET (** name do input **)
     city = city.lower()
-    # FAZER A REQUISIÇÃO DA CIDADE PEDIDA...
 
 
 if __name__ == '__main__':
---
+++ weather.py
@@ -0,0 +1,27 @@
+import requests
+import os
+from dotenv import load_dotenv
+from dataclasses import dataclass
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+@dataclass
+class WeatherData:
+    '''Objeto de resposta da API'''
+    main: str
+    description: str
+    icon: str
+    temperature: float
+
+# Função de Requisição
+def get_weather_data(city):
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&lang=pt_br'
+    response = requests.get(url).json()
+    data = WeatherData(
+        main=response.get('weather')[0].get('main'), # Definição do clima, em inglês
+        description=response.get('weather')[0].get('description'),
+        icon=response.get('weather')[0].get('icon'),
+        temperature=response.get('main').get('temp')
+    )
+    return data
