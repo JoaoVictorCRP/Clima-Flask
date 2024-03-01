d83a4c13e29a4ee563a15bb29b621e757f086435
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-03-01 07:19:27 -0300

    fazer requisicao

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import requests
 import string
 from flask import Flask, render_template, request, redirect, url_for, flash
-# from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
@@ -10,12 +9,17 @@ API_KEY = os.getenv('API_KEY')
 def get_weather_data(city):
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&lang=pt_br'
     response = requests.get(url).json()
-    print(response)
+    # print(response)
     return response
-
-get_weather_data('London')
 
 # Rota Principal
 @app.route('/')
 def get_index():
     return render_template('index.html')
+
+# Rota de Post
+def index_post():
+    msg_erro = ''
+    city = request.form.get('cidade') # GET (** name do input **)
+    city = city.lower()
+    # FAZER A REQUISIÇÃO DA CIDADE PEDIDA...
