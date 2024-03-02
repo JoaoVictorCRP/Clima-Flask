cb44773cd323f40e5fb7c6c375a1ea64337085e9
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-03-01 23:06:55 -0300

    to fix front end

--- weather.py
+++ weather.py
@@ -9,6 +9,7 @@ API_KEY = os.getenv('API_KEY')
 @dataclass
 class WeatherData:
     '''Objeto de resposta da API'''
+    name: str
     main: str
     description: str
     icon: str
@@ -19,9 +20,10 @@ def get_weather_data(city: str):
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&lang=pt_br'
     response = requests.get(url).json()
     data = WeatherData(
+        name=response.get('name'),
         main=response.get('weather')[0].get('main'), # Definição do clima, em inglês
         description=response.get('weather')[0].get('description'),
         icon=response.get('weather')[0].get('icon'),
-        temperature=response.get('main').get('temp')
+        temperature=int(response.get('main').get('temp'))
     )
     return data
