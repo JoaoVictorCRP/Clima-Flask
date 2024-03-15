54411f31b9f29d93eee97230bf915a37d977bbd8
Joao Victor <joaovictorxl120@gmail.com>
2024-03-15 17:30:06 -0300

    base para banco de dados

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy as sqla
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 import requisitions as r
 
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clima.db'
+db = sqla(app)
+
+# Modelo da cidade no banco de dados
+class City(db.Model):
+    __tablename__ = 'cities',
+    id: Mapped[int] = mapped_column(primary_key=True)
+    city: Mapped[str] = mapped_column(nullable=False)
+
+# Criando o modelo
+with app.app_context():
+    db.create_all()
 
 # Rota Principal
 @app.route('/', methods=['GET','POST'])
@@ -20,6 +36,7 @@ def index_post():
     msg_erro = ''
     city = request.form.get('cityName') # GET (** name do input **)
     city = city.lower()
+
 
 
 if __name__ == '__main__':
